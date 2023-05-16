aa63493fa95e1dfcacaa7f7414fe54ca0c7ffe33
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-17 00:13:17 +0200

    Fixed the timer

--- main.py
+++ main.py
@@ -10,16 +10,32 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def timer():
+    global reps
+    work_sec = WORK_MIN * 10
+    short_break_sec = SHORT_BREAK_MIN * 10
+    long_break_sec = LONG_BREAK_MIN * 10
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    for i in range(12):
+        reps += i
+        if reps % 2 != 0:
+            count_down(work_sec)
+        elif reps == 8:
+            count_down(long_break_sec)
+        else:
+            count_down(short_break_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_minute = math.floor(count / 60)
-    count_seconds = count%60
+    count_seconds = count % 60
 
     if count_seconds == 0:
         canvas.itemconfig(count_text, text=f"0{count_minute}:00")
@@ -27,7 +43,7 @@ def count_down(count):
         canvas.itemconfig(count_text, text=f"0{count_minute}:0{count_seconds}")
     else:
         canvas.itemconfig(count_text, text=f"0{count_minute}:{count_seconds}")
-    print(count_seconds)
+    # print(count_seconds)
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -51,7 +67,7 @@ canvas.grid(column=1, row=1)
 header = Label(text="Timer", foreground=GREEN, font=(FONT_NAME, 45, "bold"), bg=YELLOW)
 header.grid(column=1, row=0)
 
-start_button = Button(text="Start", command=lambda: count_down(5*60))
+start_button = Button(text="Start", command=lambda: timer)
 start_button.grid(column=0, row=2, ipadx=8, ipady=2)
 
 reset_button = Button(text="Reset")
